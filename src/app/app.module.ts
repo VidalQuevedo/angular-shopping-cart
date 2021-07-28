ef5857bd89bbb8a61e6377f00897459447d5de58
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2021-07-27 23:38:05 -0500

    Add HttpModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
+import { SubtotalComponent } from './subtotal/subtotal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    SubtotalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [
-    ItemsComponent
+    ItemsComponent,
+    SubtotalComponent
   ]
 })
 export class AppModule { }
